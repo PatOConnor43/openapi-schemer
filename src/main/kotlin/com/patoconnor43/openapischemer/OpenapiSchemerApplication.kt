6dee0f0ed7a5d994ead3b853db0bddb57f38f899
Pat O'Connor <patoconnor43@gmail.com>
2022-05-01 14:58:24 -0700

    Initial Commit

---
+++ src/main/kotlin/com/patoconnor43/openapischemer/OpenapiSchemerApplication.kt
@@ -0,0 +1,11 @@
+package com.patoconnor43.openapischemer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenapiSchemerApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenapiSchemerApplication>(*args)
+}
---
+++ src/test/kotlin/com/patoconnor43/openapischemer/OpenapiSchemerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.patoconnor43.openapischemer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenapiSchemerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
