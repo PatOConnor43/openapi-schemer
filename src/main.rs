d08bddacd287bdc1d5d6d856a05ff2d156978343
Pat O'Connor <patoconnor43@gmail.com>
2022-05-01 18:25:43 -0700

    Add a rough CLI app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(parse(from_os_str), value_name = "INPUT", value_hint = clap::ValueHint::DirPath, required = true)]
+    input: Option<std::path::PathBuf>,
+
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Operation(Operation),
+}
+
+#[derive(Debug, Args)]
+#[clap(args_conflicts_with_subcommands = true)]
+struct Operation {
+    #[clap(subcommand)]
+    command: OperationCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum OperationCommands {
+    /// List the operations for a spec
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("{:?}", args);
+    match args.input {
+        None => unreachable!("Clap requires input"),
+        Some(_) => match args.command {
+            Commands::Operation(subcommand) => match subcommand.command {
+                OperationCommands::List => {
+                    println!("list supplied")
+                }
+            },
+        },
+    }
+
+    ()
 }
