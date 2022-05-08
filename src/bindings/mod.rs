11710086156b29740ad6c70bad21e6ab392b9e45
Pat O'Connor <patoconnor43@gmail.com>
2022-05-08 14:18:44 -0700

    Refactor tree-sitter querying into bindings

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -15,7 +15,7 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter::{Language, Parser, Query, QueryCursor};
 
 extern "C" {
     fn tree_sitter_yaml() -> Language;
@@ -26,6 +26,108 @@ extern "C" {
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
     unsafe { tree_sitter_yaml() }
+}
+
+pub struct OperationNode {
+    pub text: String,
+}
+
+pub trait OperationParser {
+    fn get_operation_nodes(&self) -> Vec<OperationNode>;
+}
+
+pub struct TreeSitterOperationParser<'a> {
+    contents: &'a str,
+}
+impl<'a> TreeSitterOperationParser<'a> {
+    pub fn new(contents: &str) -> TreeSitterOperationParser {
+        TreeSitterOperationParser { contents }
+    }
+}
+impl<'a> OperationParser for TreeSitterOperationParser<'a> {
+    fn get_operation_nodes(&self) -> Vec<OperationNode> {
+        let mut parser = Parser::new();
+        let language = language();
+        parser.set_language(language).unwrap();
+        let tree = parser.parse(self.contents, None).unwrap();
+
+        // This query is an amalgamation of all the different supported http verbs.
+        // First we find a key with the text that matches an http verb (get/post/put...),
+        // then we look for a child `flow_node` that has the key `operationId`. If we find
+        // one we'll try to match the value of that node with should be the name of the
+        // operation.
+        let query_string = r#"
+        (block_mapping_pair
+           key: ((flow_node) @delete (eq? @delete "delete"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: (flow_node) @deletevalue (eq? @deletevalue "operationId")
+                       value: (flow_node) @deletevalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @get (eq? @get "get"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @getvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @head (eq? @head "head"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @headvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @options (eq? @options "options"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @optionsvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @patch (eq? @patch "patch"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @patchvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @post (eq? @post "post"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @postvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @put (eq? @put "put"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @putvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @connect (eq? @connect "connect"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @connectvalue
+                       ))))
+        (block_mapping_pair
+           key: ((flow_node) @trace (eq? @trace "trace"))
+           value: (block_node (block_mapping (block_mapping_pair
+                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
+                       value: (flow_node) @tracevalue
+                       ))))
+        "#;
+        let query = Query::new(language, query_string).expect("Could not construct query");
+        let mut qc = QueryCursor::new();
+        let provider = self.contents.as_bytes();
+
+        let mut entries = Vec::new();
+        for qm in qc.matches(&query, tree.root_node(), provider) {
+            if let Some(cap) = qm.captures.get(2) {
+                if let Ok(operation) = cap.node.utf8_text(provider) {
+                    entries.push(OperationNode {
+                        text: operation.to_string(),
+                    });
+                }
+            }
+        }
+        return entries;
+    }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
@@ -42,11 +144,66 @@ pub fn language() -> Language {
 
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
+
+    use crate::bindings::{OperationParser, TreeSitterOperationParser};
+
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
             .expect("Error loading yaml language");
+    }
+
+    #[test]
+    fn test_list() -> Result<(), Box<dyn Error>> {
+        let contents = r#"
+paths:
+  /pets:
+    delete:
+      operationId: deletePets
+    get:
+      summary: List all pets
+      operationId: getPets
+    post:
+      summary: Create a pet
+      operationId: postPets
+    trace:
+      operationId: tracePets
+    options:
+      operationId: optionsPets
+    put:
+      operationId: putPets
+    head:
+      operationId: headPets
+    connect:
+      operationId: connectPets
+
+  /pets/{petId}:
+    get:
+      summary: Info for a specific pet
+      operationId: showPetById
+
+            "#;
+        let parser = TreeSitterOperationParser::new(contents);
+        let result = parser.get_operation_nodes();
+        let node_texts: Vec<String> = result.into_iter().map(|node| node.text).collect();
+        assert_eq!(
+            vec![
+                "deletePets",
+                "getPets",
+                "postPets",
+                "tracePets",
+                "optionsPets",
+                "putPets",
+                "headPets",
+                "connectPets",
+                "showPetById"
+            ],
+            node_texts,
+            "Returned operations did not match expected operations"
+        );
+        Ok(())
     }
 }
--- src/operation/mod.rs
+++ src/operation/mod.rs
@@ -1,105 +1,34 @@
 use std::fmt::Display;
 
-use tree_sitter::{Parser, Query, QueryCursor};
+use crate::{
+    bindings::{self, OperationParser},
+    error::OpenapiSchemerError,
+};
 
-use crate::{bindings, error::OpenapiSchemerError};
-
-pub struct ListResult<'a> {
-    entries: Vec<&'a str>,
+pub struct ListResult {
+    entries: Vec<String>,
 }
 
-impl<'a> ListResult<'a> {
-    pub fn new(list: Vec<&str>) -> ListResult<'_> {
+impl ListResult {
+    pub fn new(list: Vec<String>) -> ListResult {
         ListResult { entries: list }
     }
 }
 
-impl Display for ListResult<'_> {
+impl Display for ListResult {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.entries.join("\n"))
     }
 }
 
 pub fn list(contents: &str) -> Result<ListResult, OpenapiSchemerError> {
-    let mut parser = Parser::new();
-    let language = bindings::language();
-    parser.set_language(language).unwrap();
-    let tree = parser.parse(contents, None).unwrap();
-
-    // This query is an amalgamation of all the different supported http verbs.
-    // First we find a key with the text that matches an http verb (get/post/put...),
-    // then we look for a child `flow_node` that has the key `operationId`. If we find
-    // one we'll try to match the value of that node with should be the name of the
-    // operation.
-    let query_string = r#"
-        (block_mapping_pair
-           key: ((flow_node) @delete (eq? @delete "delete"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: (flow_node) @deletevalue (eq? @deletevalue "operationId")
-                       value: (flow_node) @deletevalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @get (eq? @get "get"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @getvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @head (eq? @head "head"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @headvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @options (eq? @options "options"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @optionsvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @patch (eq? @patch "patch"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @patchvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @post (eq? @post "post"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @postvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @put (eq? @put "put"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @putvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @connect (eq? @connect "connect"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @connectvalue
-                       ))))
-        (block_mapping_pair
-           key: ((flow_node) @trace (eq? @trace "trace"))
-           value: (block_node (block_mapping (block_mapping_pair
-                       key: ((flow_node) @operationId (eq? @operationId "operationId"))
-                       value: (flow_node) @tracevalue
-                       ))))
-        "#;
-    let query = Query::new(language, query_string).expect("Could not construct query");
-    let mut qc = QueryCursor::new();
-    let provider = contents.as_bytes();
-
-    let mut entries = Vec::new();
-    for qm in qc.matches(&query, tree.root_node(), provider) {
-        if let Some(cap) = qm.captures.get(2) {
-            if let Ok(operation) = cap.node.utf8_text(provider) {
-                entries.push(operation);
-            }
-        }
-    }
-    Ok(ListResult::new(entries))
+    let parser = bindings::TreeSitterOperationParser::new(contents);
+    let nodes = parser.get_operation_nodes();
+    let node_texts = nodes
+        .into_iter()
+        .map(|node| node.text.to_string())
+        .collect();
+    Ok(ListResult::new(node_texts))
 }
 
 #[cfg(test)]
@@ -107,53 +36,4 @@ mod tests {
     use std::error::Error;
 
     use super::*;
-
-    #[test]
-    fn test_list() -> Result<(), Box<dyn Error>> {
-        let contents = r#"
-paths:
-  /pets:
-    delete:
-      operationId: deletePets
-    get:
-      summary: List all pets
-      operationId: getPets
-    post:
-      summary: Create a pet
-      operationId: postPets
-    trace:
-      operationId: tracePets
-    options:
-      operationId: optionsPets
-    put:
-      operationId: putPets
-    head:
-      operationId: headPets
-    connect:
-      operationId: connectPets
-
-  /pets/{petId}:
-    get:
-      summary: Info for a specific pet
-      operationId: showPetById
-
-            "#;
-        let result = list(contents)?;
-        assert_eq!(
-            vec![
-                "deletePets",
-                "getPets",
-                "postPets",
-                "tracePets",
-                "optionsPets",
-                "putPets",
-                "headPets",
-                "connectPets",
-                "showPetById"
-            ],
-            result.entries,
-            "Returned operations did not match expected operations"
-        );
-        Ok(())
-    }
 }
